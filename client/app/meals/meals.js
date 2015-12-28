8e9794d19afc6ff8d4be4a309733fc96503bc745
peterkaiserui <peterkaiserui@gmail.com>
2015-12-28 12:44:45 -0600

    working on refactor

--- client/app/meals/meals.js
+++ client/app/meals/meals.js
@@ -11,4 +11,15 @@ angular.module('tinnr.meals', [])
   				console.error('Error fetching meals: ', error);
   			});
   	}
+
+    $scope.getMealsServer = function(){
+      Meal.getSavedMeals()
+        .then(function (data){
+          $scope.meals = data;
+        })
+        .catch(function (error){
+          console.log('Error fetching meals', error);
+        });
+    }
+
   });
--- client/app/services/mealsServices.js
+++ client/app/services/mealsServices.js
@@ -4,6 +4,7 @@ angular.module('tinnr.mealsServices', [])
 
     meals.list = [];
 
+    /// These first two work
     meals.getAll = function(){
     	return meals.list;
     }
@@ -11,6 +12,36 @@ angular.module('tinnr.mealsServices', [])
     meals.saveMeal = function (meal) {
       meals.list.push(meal);
     };
+
+
+    meals.getSavedMeals = function(){
+      return $http({
+        method: 'GET',
+        url: '/api/users/getSavedMeals'
+      })
+      .then(function (res) {
+        return res.data;
+      }, function (res) {
+        console.error('Error: ', res);
+      });
+    }
+
+    meals.saveMealServer = function(meal){
+      $http({
+        method: 'POST',
+        url: '/api/users/getSavedMeals',
+        data: meal
+      })
+      .then(function (resp) {
+        /*
+        if (resp.data.token) {
+          auth.setCurrentUser(user.username, resp.data.token);
+        }
+        */
+        return resp.data.token;
+      });
+
+    }
 
     return meals; 
   });
